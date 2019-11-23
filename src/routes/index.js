b437bb22d4a345c8911165a7fc840a29074d8923
ph9ii <moattia86@gmail.com>
2019-11-23 17:48:19 +0200

    index router

---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => res.send({ title: 'Hello' }));
+
+export default router;
