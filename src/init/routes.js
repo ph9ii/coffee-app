97182717aace2664122f574722147a67b70fa55a
ph9ii <moattia86@gmail.com>
2019-11-23 18:01:58 +0200

    add /api/pods routes

--- src/init/routes.js
+++ src/init/routes.js
@@ -1,9 +1,13 @@
 import machines from '../routes/machines.router';
+import pods from '../routes/pods.router';
 import index from '../routes';
 
 export default app => {
   // Machines Router
   app.use('/api/machines', machines);
+
+  // Pods Router
+  app.use('/api/pods', pods);
 
   // Home Router
   app.use('/', index);
